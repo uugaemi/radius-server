db793f93645a37ef380d18923b3bf081441ed4f2
uugaemi <yukyum06@gmail.com>
2026-02-03 20:17:37 +0900

    [init] #1 initial settings

---
+++ src/main/java/com/uugaemi/radius/RadiusApplication.java
@@ -0,0 +1,13 @@
+package com.uugaemi.radius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uugaemi/radius/RadiusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uugaemi.radius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
